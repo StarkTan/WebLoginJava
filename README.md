9dc5302279fb807e0fb59aa159acf0ac1761bdee
Stark <386707933@qq.com>
2019-05-20 17:59:00 +0800

    1.3 允许跨域访问

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # WebLoginJava
 #####1. 使用 SpringBoot 创建项目搭建Web服务
     1. 实现页面返回(OK)
-    2. 实现Rest返回
-    3. 允许跨域访问
+    2. 实现Rest返回(OK)
+    3. 允许跨域访问(OK)
 #####2. 使用SpringSecurity搭建登录功能
     1. 登录退出功能
         Session Cookie 时效配置
---
+++ src/main/java/com/stark/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.stark.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Author : StarkTan
+ * CreateTime : 2019/5/20
+ * WebMvcConfig
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
